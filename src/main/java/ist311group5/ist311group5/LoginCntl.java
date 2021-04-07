7e38377f5521fdbabbe4d8da4d34d88a468fb0da
Bryce Ciemiewicz <bzc5373@psu.edu>
2021-04-07 11:28:08 -0400

    - Moved login UI to loginGUI class - Login now handled by LoginCntl - Fixed password label in login UI

--- src/main/java/ist311group5/ist311group5/LoginCntl.java
+++ src/main/java/ist311group5/ist311group5/LoginCntl.java
@@ -3,8 +3,33 @@ package ist311group5.ist311group5;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import java.io.File;
+import java.io.FileNotFoundException;
+        
 
 public class LoginCntl {
+    private LoginGUI loginUI;
+    public LoginCntl(Stage stage) {
+        loginUI = new LoginGUI(stage, this);
+    }
+    
     public boolean authenticate(String userName, String password) {
         String file = "userList.txt";
         try {
