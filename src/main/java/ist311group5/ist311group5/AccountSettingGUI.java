5e79dda32c64e4c8763dcbeb4393a790886acd14
dhaas01 <77082867+dhaas01@users.noreply.github.com>
2021-04-11 17:53:06 -0400

    Update AccountSettingGUI.java

--- src/main/java/ist311group5/ist311group5/AccountSettingGUI.java
+++ src/main/java/ist311group5/ist311group5/AccountSettingGUI.java
@@ -13,6 +13,9 @@ import javafx.event.EventHandler;
 import javafx.scene.paint.Color;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 
 public class AccountSettingGUI {
     private AccountSettingCntl accountSettingCntl;
@@ -20,11 +23,19 @@ public class AccountSettingGUI {
     private GridPane rootPane;
     private Scene scene;
     private Text sceneTitle;
+    private Label currentPassword;
+    private TextField currentPasswordText;
+    private Label newPassword;
+    private TextField newPasswordText;
+    private Label retypeNewPassword;
+    private TextField retypeNewPasswordText;
+            
     public AccountSettingGUI(Stage stage, AccountSettingCntl accountSettingCntl) {
         this.stage = stage;
         this.accountSettingCntl = accountSettingCntl;
         setupUI();
         setupBackButton();
+        setupSaveButton();
     }
     
     private void setupUI() {
@@ -32,22 +43,137 @@ public class AccountSettingGUI {
         scene = new Scene(rootPane, 630, 480);
         stage.setScene(scene);
         rootPane.setAlignment(Pos.CENTER);
-        rootPane.setHgap(10);
+        rootPane.setHgap(5);
         rootPane.setVgap(10);
         rootPane.setPadding(new Insets(25, 25, 25, 25));
-
-        sceneTitle = new Text("Account Settings");
-        rootPane.add(sceneTitle, 0, 0, 2, 1);
+        
+        sceneTitle = new Text("Change Password");
+        Font font = Font.font("Verdana", FontWeight.BOLD, FontPosture.REGULAR, 15);
+        sceneTitle.setFont(font);
+        rootPane.add(sceneTitle, 0, 0, 1, 1);
+        
+        currentPassword = new Label("Enter Current Password:");
+        rootPane.add(currentPassword, 0, 2);
+        
+        currentPasswordText = new TextField();
+        rootPane.add(currentPasswordText, 1, 2);
+        
+        newPassword = new Label("Enter New Password:");
+        rootPane.add(newPassword, 0, 3);
+        
+        newPasswordText = new TextField();
+        rootPane.add(newPasswordText, 1, 3);
+                
+        retypeNewPassword = new Label("Retype New Password:");
+        rootPane.add(retypeNewPassword, 0, 4);
+        
+        retypeNewPasswordText = new TextField();
+        rootPane.add(retypeNewPasswordText, 1, 4);
     }
     
     private void setupBackButton() {
-        Button backButton = new Button("Back");
-        rootPane.add(backButton, 1, 3);
+        Button backButton = new Button("Discard");
+        rootPane.add(backButton, 0, 5);
         backButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e) {
-                accountSettingCntl.changeControl("Back");
+                accountSettingCntl.changeControl("Discard");
             }
         });
+    }
+    
+    private void setupSaveButton() {
+        Button saveButton = new Button("Save");
+        rootPane.add(saveButton, 1, 5);
+        saveButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                accountSettingCntl.changeControl("Save");
+            }
+        });
+    }
+
+    /**
+     * @return the currentPassword
+     */
+    public Label getCurrentPassword() {
+        return currentPassword;
+    }
+
+    /**
+     * @param currentPassword the currentPassword to set
+     */
+    public void setCurrentPassword(Label currentPassword) {
+        this.currentPassword = currentPassword;
+    }
+
+    /**
+     * @return the currentPasswordText
+     */
+    public TextField getCurrentPasswordText() {
+        return currentPasswordText;
+    }
+
+    /**
+     * @param currentPasswordText the currentPasswordText to set
+     */
+    public void setCurrentPasswordText(TextField currentPasswordText) {
+        this.currentPasswordText = currentPasswordText;
+    }
+
+    /**
+     * @return the newPassword
+     */
+    public Label getNewPassword() {
+        return newPassword;
+    }
+
+    /**
+     * @param newPassword the newPassword to set
+     */
+    public void setNewPassword(Label newPassword) {
+        this.newPassword = newPassword;
+    }
+
+    /**
+     * @return the newPasswordText
+     */
+    public TextField getNewPasswordText() {
+        return newPasswordText;
+    }
+
+    /**
+     * @param newPasswordText the newPasswordText to set
+     */
+    public void setNewPasswordText(TextField newPasswordText) {
+        this.newPasswordText = newPasswordText;
+    }
+
+    /**
+     * @return the retypeNewPassword
+     */
+    public Label getRetypeNewPassword() {
+        return retypeNewPassword;
+    }
+
+    /**
+     * @param retypeNewPassword the retypeNewPassword to set
+     */
+    public void setRetypeNewPassword(Label retypeNewPassword) {
+        this.retypeNewPassword = retypeNewPassword;
+    }
+
+    /**
+     * @return the retypeNewPasswordText
+     */
+    public TextField getRetypeNewPasswordText() {
+        return retypeNewPasswordText;
+    }
+
+    /**
+     * @param retypeNewPasswordText the retypeNewPasswordText to set
+     */
+    public void setRetypeNewPasswordText(TextField retypeNewPasswordText) {
+        this.retypeNewPasswordText = retypeNewPasswordText;
     }
 }
