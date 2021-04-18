6794e186c5897ef17437125b98ff251871f6009f
Bryce Ciemiewicz <bzc5373@psu.edu>
2021-04-17 22:37:18 -0400

    Cleaned up class and added comments.

--- src/main/java/ist311group5/ist311group5/GUI.java
+++ src/main/java/ist311group5/ist311group5/GUI.java
@@ -15,11 +15,49 @@ public abstract class GUI {
     GridPane rootPane;
     Stage stage;
     Scene scene;
+    boolean isDarkTheme;
     
     abstract public void setupUI();
     
+    /**
+     * Sets up the font for the title.
+     */
     public void setupFont() {
         titleFont = Font.font("Verdana", FontWeight.BOLD, FontPosture.REGULAR, TITLE_TEXT_SIZE);
         sceneTitle.setFont(titleFont);
+    }
+    
+    /**
+     * Sets the title of the application.
+     */
+    public void setTitle() {
+        stage.setTitle("Chat Application");
+    }
+    
+    /**
+     * Determines the theme to use when rendering views.
+     */
+    public void setTheme() {
+        if (isDarkTheme) {
+            rootPane.getStylesheets().setAll(getClass().getResource("dark-theme.css").toExternalForm());
+        } else {
+            rootPane.getStylesheets().setAll(getClass().getResource("caspian.css").toExternalForm());
+        } 
+    }
+    
+    /**
+     * Gets the active theme.
+     * @return isDarkTheme Determines weather to use dark or light theme. 
+     */
+    public boolean getTheme() {
+        return isDarkTheme;
+    }
+    
+    /**
+     * Sets the active theme.
+     * @param isDarkTheme Sets weather to use dark or light theme.
+     */
+    public void setTheme(boolean isDarkTheme) {
+        this.isDarkTheme = isDarkTheme;
     }
 }
